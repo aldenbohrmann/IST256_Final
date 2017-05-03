7dfc88f82021889beee542ab3f495b4215cef304
Alden Bohrmann <acbohrma@syr.edu>
2017-05-02 21:30:51 -0400

    Update Final Project 2.py

--- Final Project 2.py
+++ Final Project 2.py
@@ -12,38 +12,12 @@ r = requests.get(url)
 # Turn the HTML into a Beautiful Soup object
 soup = BeautifulSoup(r.text, "html.parser")
 
-date = []
-sport = []
-event = []
-location = []
-columns = {}
-
 table = soup.find('table')
+rows = table.findAll('tr')
+data = [[td.findChildren(text=True) for td in tr.findAll("td")] for tr in rows]
 
-for row in table.find_all('table'):
-    # Create a variable of all the <td> tag pairs in each <tr> tag pair,
-    col = row.find_all('td')[:0]
+final_list = 
 
-    column_1 = col[0].string.strip()
-    # and append it to date variable
-    date.append(column_1)
-
-    column_2 = col[1].string.strip()
-    # and append it to sport variable
-    sport.append(column_2)
-
-    column_3 = col[2].string.strip()
-    # and append it to event variable
-    event.append(column_3)
-
-    column_4 = col[3].string.strip()
-    # and append it to location variable
-    location.append(column_4)
-
-    columns = {'date': date, 'sport': sport, 'event': event, 'location': location}
-
-    df = pd.DataFrame(columns)
-
-    print(df)
-
-print(table)
+for w in words[]
+    
+print(data)
